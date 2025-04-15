0a59651b6a227f0a92a4d5ebb9eb396b0ae94bf8
Ryo Uchiyama <ryo.uchiyama12@gmail.com>
2025-04-16 00:30:22 +0900

    diagnosticの表示変更

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,6 +4,21 @@ vim.keymap.set('n', ']',  '<cmd>lua vim.lsp.buf.definition()<CR>')  -- jump to d
 vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')  -- show list where the variable cursor on is referenced
 vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')      -- rename the variable
 
+-- diagnostics (See `:h vim.diagnostic.config`)
+vim.diagnostic.config({
+    underline = true,
+    virtual_text = false,
+    signs = false,
+    update_in_insert = false,
+})
+vim.cmd([[
+    highlight DiagnosticUnderlineError gui=undercurl
+    highlight DiagnosticUnderlineWarn gui=undercurl
+    highlight DiagnosticUnderlineInfo gui=undercurl
+    highlight DiagnosticUnderlineHint gui=undercurl
+]])
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)         -- open diagnostic in float
+
 return {
     "neovim/nvim-lspconfig",
     version = "*",
