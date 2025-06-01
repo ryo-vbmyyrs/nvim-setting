d2c04d1c649c01cdc4331294bef94cf33fd65648
Ryo Uchiyama <ryo.uchiyama12@gmail.com>
2025-06-01 17:25:51 +0900

    補完のwindow設定を変更

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -45,8 +45,8 @@ return {
                 end,
             },
             window = {
-                -- completion = cmp.config.window.bordered(),
-                -- documentation = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
                 ["<C-Space>"] = function()
