1dd5a3d462035f2924038d7cc257d5f93d1e4867
Ryo Uchiyama <ryo.uchiyama12@gmail.com>
2025-06-15 04:06:56 +0900

    かっこのペア自動補完プラグインの導入

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,27 @@
+return {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+        require('nvim-autopairs').setup({})
+
+        local cmp = require('cmp')
+        local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+        local handlers = require('nvim-autopairs.completion.handlers')
+        cmp.event:on(
+            'confirm_done',
+            cmp_autopairs.on_confirm_done({
+                filetypes = {
+                    ['*'] = {
+                        ['('] = {
+                            kind = {
+                                cmp.lsp.CompletionItemKind.Function,
+                                cmp.lsp.CompletionItemKind.Method,
+                            },
+                            handler = handlers['*'],
+                        },
+                    },
+                },
+            })
+        )
+    end
+}
