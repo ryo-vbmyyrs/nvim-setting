8061b9e07d7e52ee72438d046d12c02433426a4a
Ryo Uchiyama <ryo.uchiyama12@gmail.com>
2026-01-19 07:25:20 +0900

    treesitter-contextの行数が多くて鬱陶しかったので減らした

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -2,5 +2,13 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     event = { 'VeryLazy' },
-    opts = {},
+    config = function()
+        require('treesitter-context').setup({
+            multiline_threshold = 1,
+        })
+
+        vim.keymap.set('n', '<leader>j', function()
+            require('treesitter-context').go_to_context(vim.v.count1)
+        end, { silent = true })
+    end,
 }
