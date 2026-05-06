ffd5d40e33894e237daf3060f6bae3bb7413b7c4
Ryo Uchiyama <ryo.uchiyama12@gmail.com>
2026-05-07 06:27:58 +0900

    luaでnvimAPIの補完が効くようにする

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -23,7 +23,6 @@ return {
     'hrsh7th/nvim-cmp',
     dependencies = {
         { 'hrsh7th/cmp-nvim-lsp', lazy = true },
-        { 'hrsh7th/cmp-nvim-lua', lazy = true },
         { 'hrsh7th/cmp-buffer', lazy = true },
         { 'hrsh7th/cmp-path', lazy = true },
         { 'hrsh7th/cmp-cmdline', lazy = true },
