637a1623b6a5dbf09e81cc8bd18ba76e42c4261c
Ryo Uchiyama <ryo.uchiyama12@gmail.com>
2026-07-02 11:06:43 +0900

    バッファの並び替えができるようにする

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,8 +1,10 @@
 -- keymaps
 vim.keymap.set('n', 'gb', '<cmd>BufferLinePick<CR>') -- choose buffer by appearing character to open
 vim.keymap.set('n', 'gd', '<cmd>BufferLinePickClose<CR>') -- choose buffer by appearing character to close
-vim.keymap.set('n', '<C-l>', '<cmd>bnext<CR>') -- move to next buffer
-vim.keymap.set('n', '<C-h>', '<cmd>bprev<CR>') -- move to previous buffer
+vim.keymap.set('n', '<C-l>', '<cmd>BufferLineCycleNext<CR>') -- move to next buffer (visual order)
+vim.keymap.set('n', '<C-h>', '<cmd>BufferLineCyclePrev<CR>') -- move to previous buffer (visual order)
+vim.keymap.set('n', '<A-l>', '<cmd>BufferLineMoveNext<CR>') -- reorder: move current buffer right
+vim.keymap.set('n', '<A-h>', '<cmd>BufferLineMovePrev<CR>') -- reorder: move current buffer left
 vim.keymap.set('n', '<leader>ql', '<cmd>BufferLineCloseRight<CR>') -- close all buffer right of current buffer
 vim.keymap.set('n', '<leader>qh', '<cmd>BufferLineCloseLeft<CR>') -- close all buffer left of current buffer
 
